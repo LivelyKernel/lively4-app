9468564d37d496e5976dfa526cfdff653e4a2f1e
Benjamin Feldmann <benjamin-feldmann@web.de>
2017-11-21 15:27:21 +0100

    server works now

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const url = require('url');
+const spawn = require('child_process').spawn;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let win;
 
+function startElectron() {
+  createServer();
+  setTimeout(function () {
+    createWindow();
+  }, 1000);
+}
+
+function createServer() {
+  var t = spawn('node', ['lively4-server/dist/httpServer.js',
+    '--server=lively4-server/',
+    '--port=8080',
+    '--index-files=true',
+    '--directory=lively4/',
+    '--auto-commit=true']);
+
+  t.on('data', function (data) {
+    console.log(data.toString());
+  });
+
+  t.stdout.pipe(process.stdout);
+  t.stderr.pipe(process.stderr);
+}
+
 function createWindow() {
   // Create the browser window.
-  win = new BrowserWindow({  
+  win = new BrowserWindow({
     webPreferences: {
       nodeIntegration: false
     }
@@ -33,27 +57,27 @@ function createWindow() {
     // in an array if your app supports multi windows, this is the time
     // when you should delete the corresponding element.
     win = null;
-});
+  });
 }
 
 // This method will be called when Electron has finished
 // initialization and is ready to create browser windows.
 // Some APIs can only be used after this event occurs.
-app.on('ready', createWindow);
+app.on('ready', startElectron);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
   if (process.platform !== 'darwin') {
-  app.quit();
-}
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
   if (win === null) {
-  createWindow()
-}
+    createWindow()
+  }
 });
