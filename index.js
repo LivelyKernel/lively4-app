3f427bdbebdc2e1bd7e904448ee22ed72be4490c
Marcel Jankrift <marcel@jankrift.de>
2017-12-05 14:53:33 +0100

    add before quit event

--- index.js
+++ index.js
@@ -100,6 +100,10 @@ app.on('window-all-closed', () => {
     }
 });
 
+app.on('before-quit', () => {
+    app.exit();
+});
+
 app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
