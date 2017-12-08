ac340d4ac9cc8bcb18ef2d3feee810db18ddb32a
Benjamin Feldmann <benjamin-feldmann@web.de>
2017-12-08 14:33:02 +0100

    change server node process to require(httpServer);

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const {app, BrowserWindow, dialog} = require('electron');
-const path = require('path');
-const url = require('url');
-const spawn = require('child_process').spawn;
+const { app, BrowserWindow, dialog } = require('electron');
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let webWindow;
-let serverProcess;
+let server;
 
 function startElectron() {
-    startServer();
-    createWebWindow();
+  setTimeout(startServer, 0);
+  setTimeout(createWebWindow, 1000);
 }
 
 /**
@@ -18,60 +15,53 @@ function startElectron() {
  *
  * the server (and window) gets automatically killed if the webWindow is closed
  * */
- function startServer() {
-    serverProcess = spawn('node', ['lively4-server/dist/httpServer.js',
-        '--server=lively4-server/',
-        '--port=8080',
-        '--index-files=true',
-        '--directory=lively4/',
-        '--auto-commit=true']);
+function startServer() {
 
-    serverProcess.on('data', function (data) {
-        console.log(data.toString());
-    });
+  process.argv.push(
+      '--server=lively4-server/',
+      '--port=8080',
+      '--index-files=true',
+      '--directory=lively4/',
+      '--auto-commit=true');
 
-    serverProcess.stdout.pipe(process.stdout);
-    serverProcess.stderr.pipe(process.stderr);
+  server = require('./lively4-server/dist/httpServer');
+  server.start();
 }
 
 function createWebWindow() {
-    // Create the browser window.
-    webWindow = new BrowserWindow({
-        webPreferences: {
-            nodeIntegration: false
-        },
-        width: 1400,
-        height: 800
-    });
+  // Create the browser window.
+  webWindow = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: false
+    },
+    width: 1400,
+    height: 800
+  });
 
-    webWindow.loadURL('http://localhost:8080/lively4-core/start.html');
+  webWindow.loadURL('http://localhost:8080/lively4-core/start.html');
 
 
-    // Open the DevTools.
-    webWindow.webContents.openDevTools();
+  // Open the DevTools.
+  webWindow.webContents.openDevTools();
 
-    webWindow.on('close', (e) => {
-        var choice = dialog.showMessageBox(
-            {
-                type: 'question',
-                buttons: ['Yes', 'No'],
-                title: 'Confirm',
-                message: 'Are you sure you want to quit?'
-            }
-        );
-        if(choice == 1){
-            e.preventDefault();
-        }
-    });
+  webWindow.on('close', (e) => {
+    var choice = dialog.showMessageBox(
+      {
+        type: 'question',
+        buttons: ['Yes', 'No'],
+        title: 'Confirm',
+        message: 'Are you sure you want to quit?'
+      }
+    );
+    if (choice == 1) {
+      e.preventDefault();
+    }
+  });
 
-    // Emitted when the window is closed.
-    webWindow.on('closed', () => {
-        if (!serverProcess.killed) {
-            console.log("stopping server.");
-            serverProcess.kill();
-        }
-        webWindow = null;
-    });
+  // Emitted when the window is closed.
+  webWindow.on('closed', () => {
+    webWindow = null;
+  });
 }
 
 // init
@@ -79,17 +69,17 @@ app.on('ready', startElectron);
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-    // On macOS it is common for applications and their menu bar
-    // to stay active until the user quits explicitly with Cmd + Q
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
+  // On macOS it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
 });
 
 app.on('activate', () => {
-    // On macOS it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open.
-    if (webWindow === null) {
-        startElectron();
-    }
+  // On macOS it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (webWindow === null) {
+    startElectron();
+  }
 });
